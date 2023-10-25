d4a646c4f429f983e84fc2fd099fac74cb543636
Timo Hromadka <timoh@LAPTOP-J48RR0L7>
2023-10-25 17:12:54 +0100

    Re-arrange code lightly

--- run_extractive_summarizer.py
+++ run_extractive_summarizer.py
@@ -3,29 +3,31 @@ import json
 import tqdm
 from models.extractive_summarizer import ExtractiveSummarizer
 
-args = argparse.ArgumentParser()
-args.add_argument('--train_data', type=str, default='data/train.greedy_sent.json')
-args.add_argument('--eval_data', type=str, default='data/validation.json')
-args = args.parse_args()
+from args import parser
 
-model = ExtractiveSummarizer()
+def main():
+    args = parser.parse_args()
 
-with open(args.train_data, 'r') as f:
-    train_data = json.load(f)
+    model = ExtractiveSummarizer()
 
-train_articles = [article['article'] for article in train_data]
-train_highligt_decisions = [article['greedy_n_best_indices'] for article in train_data]
+    with open(args.train_data, 'r') as f:
+        train_data = json.load(f)
 
-preprocessed_train_articles = model.preprocess(train_articles)
-model.train(preprocessed_train_articles, train_highligt_decisions)
+    train_articles = [article['article'] for article in train_data]
+    train_highlight_decisions = [article['greedy_n_best_indices'] for article in train_data]
 
-with open(args.eval_data, 'r') as f:
-    eval_data = json.load(f)
+    preprocessed_train_articles = model.preprocess(train_articles)
+    model.train(preprocessed_train_articles, train_highlight_decisions)
 
+    with open(args.eval_data, 'r') as f:
+        eval_data = json.load(f)
 
-eval_articles = [article['article'] for article in eval_data]
-preprocessed_eval_articles = model.preprocess(eval_articles)
-summaries = model.predict(preprocessed_eval_articles)
-eval_out_data = [{'article': article, 'summary': summary} for article, summary in zip(eval_articles, summaries)]
+    eval_articles = [article['article'] for article in eval_data]
+    preprocessed_eval_articles = model.preprocess(eval_articles)
+    summaries = model.predict(preprocessed_eval_articles)
+    eval_out_data = [{'article': article, 'summary': summary} for article, summary in zip(eval_articles, summaries)]
 
-print(json.dumps(eval_out_data, indent=4))
+    print(json.dumps(eval_out_data, indent=4))
+
+if __name__ == "__main__":
+    main()
