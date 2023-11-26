5936cb3b02c19119f369219e5e1d3ab1cfcb8009
Timo Hromadka <timoh@LAPTOP-J48RR0L7>
2023-11-26 23:45:32 +0000

    Add boilerplate code for transformer architecture, as well as all auxiliary functions.

--- models/abstractive_summarizer.py
+++ models/abstractive_summarizer.py
@@ -1,15 +1,91 @@
-import tqdm
-import random
-import torch
+from collections import defaultdict, Counter
+import logging
+import math
 import numpy as np
 import os
+import re
+import json
+from tempfile import TemporaryDirectory
+from typing import Tuple
+
+from datasets import load_metric
+import torch
+from torch import nn, Tensor
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.nn.utils.rnn import pad_sequence
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from torch.utils.data import dataset
+from tqdm import tqdm
+
+from positional_encoding import PositionalEncoding
+from bpe import Encoder
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 class AbstractiveSummarizer:
+    
+    def __init__(
+        self, 
+        ntoken: int, 
+        d_model: int, 
+        nhead: int, 
+        d_hid: int, 
+        nlayers: int, 
+        dropout: float = 0.1):
+        
+        super(AbstractiveSummarizer, self).__init__()
+        self.SOS = '<sos>'
+        self.EOS = '<eos>'
+        self.model_type = 'Transformer'
+        self.d_model = d_model
+        self.embedding = nn.Embedding(ntoken, d_model)
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        self.model = nn.Transformer(
+            d_model, 
+            nhead, 
+            nlayers, 
+            nlayers, 
+            d_hid, 
+            dropout)
+        self.linear = nn.Linear(d_model, ntoken)
 
-    model = None
+        self.init_weights()
+        
+        
+    def init_weights(self):
+        initrange = 0.1
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.linear.bias.data.zero_()
+        self.linear.weight.data.uniform_(-initrange, initrange)
+       
+       
+    def init_vocabulary(self, train_articles, method='bpe', vocab_size=4000, vocab_file='vocab.json'):
+        """
+        X: list of articles (i.e., list of list of sentences)
+        vocab_size: desired size of the vocabulary
+        """
+        if method == 'bpe':
+            logger.info('Tokenizing Dataset with BPE Encoding.')
+            encoder = Encoder(
+                vocab_size=vocab_size, 
+                #pct_bpe=0.88,
+                silent=False
+            )
+            text = [word for article in train_articles for word in article.split()]
+            encoder.fit(text)
+            self.tokenizer = encoder
+            logger.info('Done!')
 
-    def __init__(self):
-        self.model = None # TODO: Replace with your PyTorch model
+    def forward(self, src, tgt, src_mask=None, tgt_mask=None, memory_mask=None, src_key_padding_mask=None, tgt_key_padding_mask=None, memory_key_padding_mask=None):
+        src = self.embedding(src) * math.sqrt(self.d_model)
+        tgt = self.embedding(tgt) * math.sqrt(self.d_model)
+        src = self.pos_encoder(src)
+        tgt = self.pos_encoder(tgt)
+        output = self.model(src, tgt, src_mask, tgt_mask, memory_mask, src_key_padding_mask, tgt_key_padding_mask, memory_key_padding_mask)
+        output = self.linear(output)
+        return output
         
     def train(self, X, y, val_X, val_y, learning_rate=0.001, batch_size=32, grad_acc=1, num_epochs=10, keep_best_n_models=2):
         """
@@ -74,30 +150,68 @@ class AbstractiveSummarizer:
         best_model_index = np.argmax(best_model_scores)
         self.model.load_state_dict(torch.load(best_model_paths[best_model_index]))
 
+    def tokenize(self, text):
+        """
+        Tokenizes the input text.
+        text: a string representing a sentence
+        Returns a list of tokens
+        """
+        tokenizer = get_tokenizer('basic_english')
+        return [self.vocab[token] for token in tokenizer(text)]
+
 
     def preprocess(self, X, y):
         """
-        X: list sentences (i.e., articles)
-        y: list of sentences (i.e., summaries)
+        Preprocesses the input data by tokenizing, converting to indices, padding, and adding special tokens.
+        X: list of sentences (articles)
+        y: list of sentences (summaries)
+        Returns a list of tuples, each containing a pair of torch tensors (article, summary)
         """
+        # Tokenize the input sentences and add special tokens
+        tokenized_X = [[self.SOS] + self.tokenizer.tokenize(article) + [self.EOS] for article in X]
+        tokenized_y = [[self.SOS] + self.tokenizer.tokenize(summary) + [self.EOS] for summary in y]
 
-        # TODO: Implement me!
+        # Convert tokens to indices
+        indexed_X = [self.tokenizer.transform(article) for article in tokenized_X]
+        indexed_y = [self.tokenizer.transform(summary) for summary in tokenized_y]
+
+        # Pad and truncate the sequences to a fixed length
+        # Accounting for the <sos> and <eos> tokens
+        max_length = self.d_model - 2
+        padded_indexed_X = [article[:max_length] + [self.EOS] + [self.tokenizer.PAD]*(max_length - len(article)) if len(article) < max_length else article[:max_length] + [self.EOS] for article in indexed_X]
+        padded_indexed_y = [summary[:max_length] + [self.EOS] + [self.tokenizer.PAD]*(max_length - len(summary)) if len(summary) < max_length else summary[:max_length] + [self.EOS] for summary in indexed_y]
+
+        # Combine the articles and summaries into pairs
+        dataset = list(zip(padded_indexed_X, padded_indexed_y))
+
+        return dataset
+
+        
 
     def compute_loss(self, batch):
         """
         batch: tensor of token indices. Dimensionality is (batch_size, sequence_length)
         """
+        batch_X, batch_y = zip(*batch)
+        batch_X = torch.stack(batch_X)
+        batch_y = torch.stack(batch_y)
 
-        # TODO: Implement me!
+        outputs = self.model(batch_X)
+        loss_fn = nn.CrossEntropyLoss()
+        loss = loss_fn(outputs.view(-1, outputs.size(-1)), batch_y.view(-1))
+
+        return loss
+
 
     def generate(self, X):
         """
         X: list of sentences (i.e., articles)
         """
+        tokenized_X = [torch.tensor([self.vocab.stoi[token] for token in article.split()[:self.max_len]]) for article in X]
+        padded_X = pad_sequence(tokenized_X, batch_first=True)
 
-        self.model.eval()
-        
-        # TODO: Implement me!
+        outputs = self.model(padded_X)
+        return torch.argmax(outputs, dim=-1)
 
 
     def decode(self, tokens):
@@ -105,7 +219,7 @@ class AbstractiveSummarizer:
         tokens: list of token indices
         """
 
-        # TODO: Implement me!
+        return [self.vocab.itos[token] for token in tokens]
 
     def compute_validation_score(self, X, y):
         """
@@ -115,7 +229,12 @@ class AbstractiveSummarizer:
 
         self.model.eval()
 
-        # TODO: Implement me. You can compute either loss or ROUGE.
+        # compute either loss or ROUGE
+        metric = load_metric('rouge')
+        predictions = self.predict(X)
+        metric.add_batch(predictions=predictions, references=y)
+        score = metric.compute()
+        return score
 
     def predict(self, X, k=3):
         """
@@ -132,3 +251,26 @@ class AbstractiveSummarizer:
             summary = ' . '.join(output_tokens)
             
             yield summary
+            
+            
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
--- run_abstractive_summarizer.py
+++ run_abstractive_summarizer.py
@@ -1,34 +1,61 @@
 import argparse
 import json
+
+from torchtext.datasets import WikiText2
+
 from models.abstractive_summarizer import AbstractiveSummarizer
 
-args = argparse.ArgumentParser()
-args.add_argument('--train_data', type=str, default='data/train.json')
-args.add_argument('--validation_data', type=str, default='data/validation.json')
-args.add_argument('--eval_data', type=str, default='data/test.json')
-args = args.parse_args()
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument('--train_data', type=str, default='data/train.json')
+    args.add_argument('--validation_data', type=str, default='data/validation.json')
+    args.add_argument('--test_data', type=str, default='data/test.json')
+    args = args.parse_args()
+    
+    hparams = {
+        # "train_data": "data/train.json",
+        # "validation_data": "data/validation.json",
+        # "eval_data": "data/test.json",
+        'ntoken': 128,
+        'd_model': 128,
+        'nhead': 4,
+        'd_hid': 512,
+        'nlayers': 2,
+        'dropout': 0.1
+    }
 
-model = AbstractiveSummarizer()
+    model = AbstractiveSummarizer(
+        hparams['ntoken'], 
+        hparams['d_model'], 
+        hparams['nhead'], 
+        hparams['d_hid'], 
+        hparams['nlayers'], 
+        hparams['dropout']
+        )
 
-with open(args.train_data, 'r') as f:
-    train_data = json.load(f)
+    with open(args.train_data, 'r') as f:
+        train_data = json.load(f)
 
-with open(args.validation_data, 'r') as f:
-    validation_data = json.load(f)
+    with open(args.validation_data, 'r') as f:
+        validation_data = json.load(f)
 
-train_articles = [article['article'] for article in train_data]
-train_summaries = [article['summary'] for article in train_data]
+    train_articles = [article['article'] for article in train_data]
+    train_summaries = [article['summary'] for article in train_data]
 
-val_articles = [article['article'] for article in validation_data]
-val_summaries = [article['summary'] for article in validation_data]
+    val_articles = [article['article'] for article in validation_data]
+    val_summaries = [article['summary'] for article in validation_data]
 
-model.train(train_articles, train_summaries, val_articles, val_summaries)
+    model.init_vocabulary(train_articles)
+    model.train(train_articles, train_summaries, val_articles, val_summaries)
 
-with open(args.eval_data, 'r') as f:
-    eval_data = json.load(f)
+    with open(args.test_data, 'r') as f:
+        test_data = json.load(f)
 
-eval_articles = [article['article'] for article in eval_data]
-summaries = model.predict(eval_articles)
-eval_out_data = [{'article': article, 'summary': summary} for article, summary in zip(eval_articles, summaries)]
+    test_articles = [article['article'] for article in test_data]
+    summaries = model.predict(test_articles)
+    test_out_data = [{'article': article, 'summary': summary} for article, summary in zip(test_articles, summaries)]
 
-print(json.dumps(eval_out_data, indent=4))
+    print(json.dumps(test_out_data, indent=4))
+
+if __name__ == '__main__':
+    main()
